408a20c5b741b57f4865a0ce30eb2c69701a6dd6
Oscillosirus <jalencullen.baker@gmail.com>
2025-12-06 20:11:39 +0800

    Fix No.1: Made Nlohmann json work properly in vscode

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -11,7 +11,6 @@ json readJsonFile(const string& filename){
   ifstream file(filename);
   json data;
   if (file.is_open()) {
-<<<<<<< HEAD
     try {
       // Use the extraction operator to read the JSON
       file >> data;
@@ -22,11 +21,6 @@ json readJsonFile(const string& filename){
       cerr << "JSON Parse Error in " << filename << ": " << e.what() 
                 << " at byte " << e.byte;
     }
-=======
-    file >> data;
-    file.close();
-    cout << "Reading Data From Json";
->>>>>>> 140f9c70c761843d23d63e6addf69f6b538db965
   } else {
     cerr << "File Could Not Be Opened: " << filename;
   }
